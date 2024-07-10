3fb367772550ef4bebc0201896d370564ac64222
gimgyumin <qmean59@gmail.com>
2024-07-11 04:28:20 +0900

    spring security ~ch07

---
+++ spirngsecurity/ch07/src/main/java/study/ch07/config/RoleConfig.java
@@ -0,0 +1,54 @@
+package study.ch07.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class RoleConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+
+        UserDetails user1 = User.withUsername("john")
+                .password("12345")
+                .roles("ADMIN")
+                .build();
+
+        UserDetails user2 = User.withUsername("jane")
+                .password("12345")
+                .roles("MANAGER")
+                .build();
+
+        manager.createUser(user1);
+        manager.createUser(user2);
+
+        return manager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.httpBasic();
+
+        http.authorizeHttpRequests(auth -> {
+            auth.anyRequest().hasRole("ADMIN");
+        });
+
+        return http.build();
+    }
+}
---
+++ spirngsecurity/ch07/src/test/java/study/ch07/Ch07ApplicationTests.java
@@ -0,0 +1,13 @@
+package study.ch07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch07ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
